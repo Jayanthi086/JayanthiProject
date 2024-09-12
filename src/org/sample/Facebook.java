80e2a812146f83df48d6ba36bcb0eb67b2c356a7
Jayanthi086 <jayanthi086@gmail.com>
2024-09-12 16:51:44 +0530

    jayanthi project

---
+++ src/org/sample/Facebook.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class Facebook {
+	public static void main(String[] args) {
+		System.out.println("login");
+		System.out.println("username");
+		System.out.println("password");
+	}
+
+}
